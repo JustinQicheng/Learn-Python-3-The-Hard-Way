e58b75d3ba7a72f935bb7593d9f527fbf9e53e79
Justin <qchding@gmail.com>
2020-12-21 21:46:14 +0800

    add ex3

---
+++ ex3.py
@@ -0,0 +1,25 @@
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100 - 25.00 * 3.00 % 4.00)
+
+print("Now I will count the eggs:")
+
+print(3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# Demonstrate that the comparison can be the second input of print function 
+print("Is is greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
