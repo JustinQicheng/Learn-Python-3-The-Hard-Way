5e698750292f9243c3dd57514df9fb2722302d4e
Justin <qchding@gmail.com>
2020-12-22 23:27:39 +0800

    add ex5.py

---
+++ ex5.py
@@ -0,0 +1,26 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+# Change inches to centimeters
+my_height_cm = my_height * 2.54
+my_weight = 180
+# Change pounds to kilograms
+my_weight_kg = my_weight * 0.454
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+print("The answers of the second question in Study Drills:")
+print(f"His height {my_height} inches equals to {my_height_cm} cm.")
+print(f"His weight {my_weight} pounds equals to {my_weight_kg} kg.")
